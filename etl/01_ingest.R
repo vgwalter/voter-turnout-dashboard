ac0d1a79ca7e7be9a5ea49b90e3280f7260bf807
vgwalter <vgwalter24@gmail.com>
2026-02-07 01:14:30 -0500

    added documentation and applied styler

--- etl/01_ingest.R
+++ etl/01_ingest.R
@@ -1,10 +1,10 @@
 library(readr)
 library(here)
 
+# load in raw data
 staged_turnout <- read_tsv(
   here("data", "raw", "icpsr", "county_turnout_icpsr.tsv")
 )
 
+# save staged data - to be loaded into 02_clean_validate
 saveRDS(staged_turnout, here("data", "processed", "staged_turnout.rds"))
-
-
--- etl/02_clean_validate.R
+++ etl/02_clean_validate.R
@@ -1,22 +1,29 @@
 library(readr)
 library(dplyr)
 library(stringr)
+library(janitor)
 library(here)
 
 
 staged_turnout <- readRDS(here("data", "processed", "staged_turnout.rds"))
 
 county_turnout <- staged_turnout %>%
-  filter(!is.na(BALLOTS_CAST), !is.na(CVAP), CVAP > 0) %>%
+  janitor::clean_names() %>%
+  # retain only county-years with sufficient data to calculcate turnout_rate metric
+  filter(
+    !is.na(ballots_cast), # reported ballot counts
+    !is.na(cvap), # reported citizen voting-age population
+    cvap > 0 # avoid division by zero
+  ) %>%
   rename(
-    county_fips = STCOFIPS10,
-    year = YEAR,
-    ballots_cast = BALLOTS_CAST,
-    cvap = CVAP
-  )
+    county_fips = stcofips10
+  ) %>%
   mutate(
+    # county turnout rate defined as ballots cast divided by citizen voting-age population (CVAP)
     turnout_rate = ballots_cast / cvap,
-    state_fips = substr(STCOFIPS10, 1, 2)
+
+    # extract two-digit state FIPS from county FIPS code
+    state_fips = str_sub(county_fips, 1, 2)
   ) %>%
   select(
     county_fips,
